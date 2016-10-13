132dd363ef66aa73555ab358582599c5a6b732f6
karenvega <karenya11@hotmail.com>
2016-10-12 22:16:56 -0500

    se eliminan clases de prueba

--- src/test/java/NorthTest.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by karenvega on 10/11/16.
- */
-public class NorthTest {
-}
--- src/test/java/RandomStrategyTest.java
+++
@@ -1,9 +0,0 @@
-/**
- * Test of the random strategy
- * Created by karenvega on 10/4/16.
- */
-public class RandomStrategyTest {
-
-
-
-}
